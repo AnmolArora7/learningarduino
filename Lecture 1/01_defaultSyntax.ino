42b235edc67c64eae4ecc886665fcea1e0313afd
Anmol Arora <anmol.arora.sml@gmail.com>
2025-06-18 18:40:38 +0530

    First day successful

---
+++ Lecture 1/01_defaultSyntax.ino
@@ -0,0 +1,16 @@
+// basic syntax:
+void setup()
+{
+
+}
+
+void loop()
+{
+    
+}
+
+
+// notes
+program runs in linear sequence
+program is case sensitive
+We have to put semi-colon(;) after every statement.
---
+++ Lecture 2/01_variables.ino
@@ -0,0 +1,18 @@
+// syntax of variable assignment:
+//  data_type variable name = value
+
+// Example of a global variable. This variable can be accessed anywhere in the code.
+int val = 15;
+
+void setup(
+    {
+        // Example of a local variable. Can only be accessed in the block it is written in. 
+        int num = 10;
+    }
+)
+
+void loop()
+{
+    // Example of a local variable. Can only be accessed in the block it is written in. Here, it is void loop:
+    int time = 5
+}
